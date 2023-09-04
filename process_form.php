1b7cce8d5e7fa2d18e66048c0a344215c8198b0c
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2023-09-04 17:10:10 +0500

    Вынес подключение к базе данных в отдельный файл и класс

--- process_form.php
+++ process_form.php
@@ -1,30 +1,17 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__); // Путь к директории, где находится .env файл
 $dotenv->load();
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
+use App\MySQLConnection;
 
 $loader = new FilesystemLoader('/');
 $twig = new Environment($loader);
 $requestUri = $_SERVER['REQUEST_URI'];
-
-class MySQLConnection {
-    public static function connect ()
-    {
-        $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']); 
-        $conn->set_charset("utf8");
-        
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        return $conn; 
-    }
-}
 
 if ($requestUri === '/') {
     $formInsert = $twig->load('index.twig');
